0927d88f61977f807b165434631699ef3d8a1ff8
AnthonyJordanDang <anthonydang8@hotmail.com>
2020-09-17 23:08:56 -0400

    Add cpp templates

---
+++ cp/template.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h> 
+using namespace std;
+using ll = long long;
+
+#define fast_io ios_base::sync_with_stdio(false) ; cin.tie(0); cout.tie(0);
+
+int main() {
+    fast_io;
+}
+
